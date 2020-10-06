eb4ceb3268f8ad38915d88e1493255c36add47c8
JensdbHowest <jens.de.bock@student.howest.be>
2020-10-06 12:23:33 +0200

    Labo3: Toggle Password vissible

--- script/app.js
+++ script/app.js
@@ -2,8 +2,25 @@ function handleFloatingLabel() {
    
 }
 
-function handlePasswordSwitcher() {
-    
+const handlePasswordSwitcher = function () {
+    const passwordInput = document.querySelector('.js-password-input'),
+        passwordToggle = document.querySelector('.js-password-toggle');
+
+    if(!passwordInput || !passwordToggle) {
+        console.error('De classes werden niet gevonden', passwordInput ,passwordToggle);
+        return;
+    }
+
+    //we gaan luisteren of er geklikt wordt op de checkbox
+    passwordToggle.addEventListener('change', function(){
+        //Als er geklikt wordt, veranderen we het type van de input van 'password' naar 'text' en vice versa
+        if (passwordInput.type == 'password') {
+            passwordInput.type = 'text';
+        }
+        else{
+            passwordInput.type = 'password';
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -11,3 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     handleFloatingLabel();
     handlePasswordSwitcher();
 });
+
+js-password-toggle
+
+js-password-input
